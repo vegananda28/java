64f666dc0e96626c48132d63e8c5115dc58b9c96
vegananda28 <151437945+vegananda28@users.noreply.github.com>
2024-05-14 19:29:24 +0700

    Add files via upload

---
+++ LoginAndSignup.java
@@ -0,0 +1,15 @@
+
+package loginandsignup;
+
+public class LoginAndSignup {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+    }
+    
+}
